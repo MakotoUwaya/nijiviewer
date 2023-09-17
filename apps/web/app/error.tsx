fa8f6def8a7c23bc8349d18db1b6472bbab568cd
oichan <makoto.uwaya@gmail.com>
2023-09-17 14:43:53 +0900

    Add web app

---
+++ apps/web/app/error.tsx
@@ -0,0 +1,32 @@
+'use client' 
+ 
+import { useEffect } from 'react'
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}): JSX.Element {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    // eslint-disable-next-line no-console -- Display error
+    console.error(error)
+  }, [error])
+ 
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      {/* eslint-disable-next-line react/button-has-type -- submit button */}
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => { reset(); }
+        }
+      >
+        Try again
+      </button>
+    </div>
+  )
+}
---
+++ apps/web/components/counter.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@nextui-org/button";
+
+export function Counter(): JSX.Element {
+	const [count, setCount] = useState(0);
+
+	return (
+		<Button onPress={() => { setCount(count + 1); }} radius="full">
+			Count is {count}
+		</Button>
+	);
+}
