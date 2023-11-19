249200ae4962442d55c7cc425d0b1d086ee268e2
oichan <makoto.uwaya@gmail.com>
2023-11-19 16:35:33 +0900

    🚨 Fix ESLint wornings

--- apps/docs/.eslintrc.js
+++ apps/docs/.eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ["custom/next"],
+  rules: {
+    "react/function-component-definition": "off",
+  },
 };
--- apps/docs/components/counters.tsx
+++ apps/docs/components/counters.tsx
@@ -1,22 +1,19 @@
+import type { FC } from "react";
 import { useState } from "react";
 import styles from "./counters.module.css";
 
-const MyButton = () => {
+const MyButton: FC = () => {
   const [count, setCount] = useState(0);
-  const handleClick = () => {
+  const handleClick = (): void => {
     setCount(count + 1);
   };
   return (
     <div>
-      <button onClick={handleClick} className={styles.counter}>
+      <button className={styles.counter} onClick={handleClick} type="button">
         Clicked {count} times
       </button>
     </div>
   );
 };
 
-const MyApp = () => {
-  return <MyButton />;
-};
-
-export default MyApp;
+export default MyButton;
