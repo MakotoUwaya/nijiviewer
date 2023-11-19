b915e399419ecb2e7f71a3fbd0d128570384ec5a
oichan <makoto.uwaya@gmail.com>
2023-11-19 21:52:52 +0900

    ✨ Add Youtube link

--- apps/web/components/videos.tsx
+++ apps/web/components/videos.tsx
@@ -10,13 +10,15 @@ export default function Videos(props: VideoProps): JSX.Element {
     <>
       {props.videos.map((v) => (
         <Card className="w-[480px] h-[316px] mx-1" isFooterBlurred key={v.id}>
-          <Image
-            alt={v.title}
-            className="z-0 w-full -translate-y-16 object-cover"
-            isZoomed
-            removeWrapper
-            src={`https://i.ytimg.com/vi/${v.id}/sddefault.jpg`}
-          />
+          <a href={`https://www.youtube.com/watch?v=${v.id}`} rel="noopener noreferrer" target="_blank">
+            <Image
+              alt={v.title}
+              className="z-0 w-full -translate-y-16 object-cover"
+              isZoomed
+              removeWrapper
+              src={`https://i.ytimg.com/vi/${v.id}/sddefault.jpg`}
+            />
+          </a>
           <CardFooter className="absolute bg-black bottom-0 p-0 z-10 border-t-1 border-default-600 dark:border-default-100">
             <div className="flex flex-col w-full px-1">
               <p className="text-tiny text-white break-words h-[32px] my-1">{v.title}</p>
