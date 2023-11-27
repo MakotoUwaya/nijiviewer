84de92ccbedfecd08cb13bde0aac8e1b847acb7d
oichan <makoto.uwaya@gmail.com>
2023-11-27 09:10:49 +0900

    💄 Fix image view size(temp)

--- apps/web/components/videos.tsx
+++ apps/web/components/videos.tsx
@@ -9,17 +9,17 @@ export default function Videos(props: VideoProps): JSX.Element {
   return (
     <>
       {props.videos.map((v) => (
-        <Card className="max-w-md h-[20vh] mx-1" isFooterBlurred key={v.id}>
+        <Card className="max-w-sm mx-1" key={v.id}>
           <a href={`https://www.youtube.com/watch?v=${v.id}`} rel="noopener noreferrer" target="_blank">
             <Image
               alt={v.title}
-              className="z-0 w-full h-[16vh] object-cover"
+              className="z-0 w-full object-cover"
               isZoomed
               removeWrapper
               src={`https://i.ytimg.com/vi/${v.id}/sddefault.jpg`}
             />
           </a>
-          <CardFooter className="absolute bg-black bottom-0 p-0 z-10">
+          <CardFooter className="bg-black bottom-0 p-0 z-10">
             <div className="flex flex-col w-full px-1">
               <p className="text-tiny text-white break-words h-[32px] my-1">{v.title}</p>
               <User
