bb8729af876813bf46ff22560ce2b9a44f3a08c0
oichan <makoto.uwaya@gmail.com>
2024-04-21 22:18:43 +0900

    ✨ Fix live-videos selector

--- apps/web/app/live-videos/page.tsx
+++ apps/web/app/live-videos/page.tsx
@@ -1,18 +1,37 @@
+'use client';
+
 import OrgSelector from '@/components/org-selector';
 import Videos from '@/components/videos';
-import { Suspense, useState } from 'react';
-import { fetchLiveVideos } from '../../lib/data';
+import { Suspense, useEffect, useState } from 'react';
 
-export default async function LiveVideosPage(): Promise<JSX.Element> {
-  const liveVideos = await fetchLiveVideos('Nijisanji');
-  console.log(liveVideos);
+import { organizations } from '@/consts/organizations';
+import type { Organization, Video } from '@/lib/holodex';
+
+export default function LiveVideosPage(): JSX.Element {
+  const [organization, setOrganization] = useState(organizations[0]);
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  useEffect(() => {
+    fetch(`/api/video/${organization.id}`)
+      .then(data => data.json())
+      .then((data) => {
+        setVideos(data)
+      })
+      .catch(e => {
+        console.error(e);
+      })
+  }, [organization]);
+
+  const onChangeOrganization = (organization: Organization) => {
+    setOrganization(organization);
+  };
   return (
     <div className='flex-col'>
       <div className='flex justify-center'>
-        <OrgSelector />
+        <OrgSelector items={organizations} defaultSelectedKeys={[organization.id]} onChange={onChangeOrganization} />
       </div>
       <Suspense fallback={<div>loading...</div>}>
-        <Videos videos={liveVideos} />
+        <Videos videos={videos} />
       </Suspense>
     </div>
   );
---
+++ apps/web/consts/organizations.ts
@@ -0,0 +1,11 @@
+import type { Organization } from "@/lib/holodex";
+
+// TODO: Add organization
+export const organizations: Organization[] = [
+  {
+    id: "Nijisanji",
+    name: "にじさんじ",
+    logoUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLSWxXsb2nHf7l5JIOhHr1G_DXAIvBTfZatmsimn=s88-c-k-c0x00ffffff-no-rj",
+  },
+];
