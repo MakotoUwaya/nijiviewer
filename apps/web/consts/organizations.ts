8b58b4fa1237b9ac886883a30067a5d44163a5d3
oichan <makoto.uwaya@gmail.com>
2024-04-21 22:28:27 +0900

    🔧 Add organizations

--- apps/web/consts/organizations.ts
+++ apps/web/consts/organizations.ts
@@ -1,11 +1,46 @@
 import type { Organization } from "@/lib/holodex";
 
-// TODO: Add organization
 export const organizations: Organization[] = [
   {
     id: "Nijisanji",
     name: "にじさんじ",
     logoUrl:
       "https://yt3.ggpht.com/ytc/AKedOLSWxXsb2nHf7l5JIOhHr1G_DXAIvBTfZatmsimn=s88-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "Hololive",
+    name: "ホロライブ",
+    logoUrl:
+      "https://yt3.ggpht.com/7ajjK5dKPtpFdAVtuZAdanfIwPhSa1tif_5TCLEApvx1J3kovTayOXGspX9FHHReP_6M-Nel=s800-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "VSpo",
+    name: "ぶいすぽ",
+    logoUrl:
+      "https://yt3.ggpht.com/dgFxRY6ACT5Qi3lM1j8YZPe_ZGH_pCRy2N_p5znoAE9mYTYYqUN64RGQvMnMqF4MDr-PKEQJyU8=s800-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "Neo-Porte",
+    name: "ネオポルテ",
+    logoUrl:
+      "https://yt3.ggpht.com/K5CdDF4n_zOM2a6vinh-jEgEWjjpA71wMVxq8kMivQ0uAA_Ulo9i3A5u2Lzmptr9F4TzrNC5tg=s800-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "Aogiri Highschool",
+    name: "あおぎり高校",
+    logoUrl:
+      "https://yt3.ggpht.com/jnl8hkoKhCS8Bg1ANRJuMC1ikvYHwd4VbkU9RouaNiwRN7LeuSu5exqjq1aGUEbdtNmA8pwxfP8=s800-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "774inc",
+    name: "774inc",
+    logoUrl:
+      "https://yt3.ggpht.com/s2hmooDjHc_RlIyE-Umagsfq2nCHp1mhFxvAiMMXNQA9xePvj74Ln1afTCRaJR7oyYIqPobN0g=s800-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "Riot Music",
+    name: "Riot Music",
+    logoUrl:
+      "https://yt3.ggpht.com/ytc/AIdro_mIHIhiEuzP6aVQM7munjgFUSY5Vw6Yi_oWG7erK8gf=s800-c-k-c0x00ffffff-no-rj",
   },
 ];
