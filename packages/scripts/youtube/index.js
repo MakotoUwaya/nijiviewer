5ca9abcb824dc77d5bb65aa28d2341b263b48dd8
oichan <makoto.uwaya@gmail.com>
2024-06-09 10:05:53 +0900

    🌱 Add Catalog sample

--- packages/scripts/youtube/index.js
+++ packages/scripts/youtube/index.js
@@ -1,29 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.local', override: true });
 
-import { watchmenBook } from './book.js';
+import _ from 'lodash';
 
-const getLatestVideoByChannelId = async (channelId, maxResults = 10) => {
-  // biome-ignore lint/style/useTemplate: <explanation>
-  const url = 'https://www.googleapis.com/youtube/v3/search' +
-    `?key=${process.env.YOUTUBE_API_KEY}` +
-    `&channelId=${channelId}` +
-    '&part=snippet,id' +
-    '&order=viewCount' +
-    `&maxResults=${maxResults}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  // url: `https://www.youtube.com/watch?v=${video.id.videoId}`
-  return data?.items;
-}
+import { watchmenBook } from './book.js';
+import { catalogData } from './catalog.js';
 
 const main = async () => {
-  console.log("Hello World");
-
-  // const list = await getLatestVideoByChannelId('UC0t_U2BgpK5LcSB9_ccuH4g', 10);
-  // console.log(list);
-
-  console.log(watchmenBook);
+  console.log(_.get(catalogData, ["booksByIsbn", "978-1779501127", "title"]));
 };
 
 await main();
