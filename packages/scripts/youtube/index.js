9b9846e6c4cee8b7136c9b61260bae069bb99539
oichan <makoto.uwaya@gmail.com>
2024-06-09 10:17:09 +0900

    🌱 Add data mapping

--- packages/scripts/youtube/index.js
+++ packages/scripts/youtube/index.js
@@ -6,8 +6,24 @@ import _ from 'lodash';
 import { watchmenBook } from './book.js';
 import { catalogData } from './catalog.js';
 
+const authorNames = (catalog, book) => {
+  const authorIds = _.get(book, "authorIds");
+  return _.map(authorIds, authorId => {
+    return _.get(catalog, ["authorsById", authorId, "name"]);
+  });
+};
+
+const bookInfo = (catalog, book) => {
+  return {
+    title: _.get(book, "title"),
+    isbn: _.get(book, "isbn"),
+    authorNames: authorNames(catalog, book),
+  };
+};
+
+
 const main = async () => {
-  console.log(_.get(catalogData, ["booksByIsbn", "978-1779501127", "title"]));
+  console.log(bookInfo(catalogData, watchmenBook));
 };
 
 await main();
