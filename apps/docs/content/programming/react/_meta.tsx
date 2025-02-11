ebe4061747b01dd426413725cbe2475148eced23
oichan <makoto.uwaya@gmail.com>
2025-02-11 17:58:25 +0900

    Fix sort docs by newer

--- apps/docs/content/programming/react/_meta.tsx
+++ apps/docs/content/programming/react/_meta.tsx
@@ -1,4 +1,5 @@
 export default {
-  '2022-01-22-snowpack-jest-v27-fix': 'Snowpack で Jest v27 にアップデートする',
+  'nextjs': 'Next.js',
   '2024-01-19-code-sample': 'Code Sample',
+  '2022-01-22-snowpack-jest-v27-fix': 'Snowpack で Jest v27 にアップデートする',
 };
