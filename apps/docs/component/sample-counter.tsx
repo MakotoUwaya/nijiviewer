a41b7fe31e53e5c9b9683c1c2ec19b34c6526184
oichan <makoto.uwaya@gmail.com>
2025-02-11 14:19:39 +0900

    Fix react code sample

---
+++ apps/docs/component/sample-counter.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Button } from 'nextra/components';
+import type { FC, ReactNode } from 'react';
+import { useState } from 'react';
+
+export const SampleCounter: FC<{ children: ReactNode }> = ({ children }) => {
+  const [count, setCount] = useState(0);
+  const handleClick = (): void => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <div className="mt-6">
+        {children}
+      </div>
+      <div className="mt-3 flex justify-center gap-3 text-sm">
+        <Button onClick={handleClick} variant="outline">
+          Clicked {count} times
+        </Button>
+      </div>
+    </>
+  );
+};
