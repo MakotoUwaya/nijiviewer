f1da69739546debafe3711e3b7dd4e3eebb5ef61
oichan <makoto.uwaya@gmail.com>
2025-03-26 14:28:48 +0900

    � NavbarのStorybookストーリーにYouTubePlayerProviderを追加

--- apps/web/components/navbar.stories.tsx
+++ apps/web/components/navbar.stories.tsx
@@ -1,10 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { YouTubePlayerProvider } from "../hooks/useYouTubePlayerContext";
 import { Navbar } from "./navbar";
 
 const meta = {
   title: "Components/Navbar",
   component: Navbar,
   tags: ["autodocs"],
+  decorators: [(Story) => {
+    return (
+      <YouTubePlayerProvider>
+        <Story />
+      </YouTubePlayerProvider>
+    );
+  }]
 } satisfies Meta<typeof Navbar>;
 
 export default meta;
