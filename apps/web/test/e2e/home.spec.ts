23029db13467a7a46d52ccac50bfb2f507b70df1
oichan <makoto.uwaya@gmail.com>
2025-03-30 19:24:58 +0900

    🔧 E2Eテストのモバイルブラウザ設定を更新し、iPhone 12からiPhone 13に変更

--- apps/web/test/e2e/home.spec.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '@/test/e2e/test-config';
-import { expect, test } from '@playwright/test';
-
-test.describe('ホーム画面のテスト', () => {
-  test('アプリタイトルが NijiViewer になっていること', async ({ page }) => {
-    await page.goto(config.baseURL);
-    await expect(page).toHaveTitle('NijiViewer');
-  });
-});
