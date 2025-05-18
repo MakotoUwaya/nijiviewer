1b9fe529ef163b0dfc66bdbcc3c0c4b11475460f
oichan <makoto.uwaya@gmail.com>
2025-05-18 15:22:39 +0900

    feat: add CookieBot component to Metrics for consent management

--- apps/web/metrics/index.tsx
+++ apps/web/metrics/index.tsx
@@ -1,10 +1,12 @@
 import GoogleAnalytics from './analytics';
 import MicrosoftClarity from './clarity';
+import CookieBot from './cookieBot';
 
 const Metrics = () => (
   <>
     <GoogleAnalytics />
     <MicrosoftClarity />
+    <CookieBot />
   </>
 );
 
