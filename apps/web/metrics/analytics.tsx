f62cad9e967c1438109048ba1c046af31b017165
oichan <makoto.uwaya@gmail.com>
2025-05-18 17:02:42 +0900

    refactor: update Google Analytics loading strategy and remove CookieBot component

--- apps/web/metrics/analytics.tsx
+++ apps/web/metrics/analytics.tsx
@@ -6,26 +6,21 @@ import Script from 'next/script';
 const GoogleAnalytics = () => {
   return (
     <>
-      <GoogleTagManager
-        gtmId={`${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}`}
-      />
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
       <Script
         id="google-analytics-init"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         // biome-ignore lint/security/noDangerouslySetInnerHtml: Google Analytics の初期化に必要
         dangerouslySetInnerHTML={{
           __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-                    `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
         }}
+      />
+      <GoogleTagManager
+        gtmId={`${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}`}
       />
     </>
   );
--- apps/web/metrics/index.tsx
+++ apps/web/metrics/index.tsx
@@ -1,12 +1,10 @@
 import GoogleAnalytics from './analytics';
 import MicrosoftClarity from './clarity';
-import CookieBot from './cookieBot';
 
 const Metrics = () => (
   <>
     <GoogleAnalytics />
     <MicrosoftClarity />
-    <CookieBot />
   </>
 );
 
