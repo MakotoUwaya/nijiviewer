42472ab2ce998faac975bde42d6f4c89f2679e5f
oichan <makoto.uwaya@gmail.com>
2025-12-31 11:35:49 +0900

    feat: add upcoming videos section and divider to the Videos component

--- apps/web/components/videos.tsx
+++ apps/web/components/videos.tsx
@@ -2,6 +2,7 @@
 
 import { DateTime } from 'luxon';
 import type { JSX } from 'react';
+import { Divider } from "@heroui/react";
 import type { Video } from '@/lib/holodex';
 import { EmptyImage } from './images';
 import ScrollToTopButton from './scroll-to-top-button';
@@ -32,7 +33,11 @@ const Videos = (props: VideoProps): JSX.Element => {
     .filter((v) => v.type !== 'clip')
     .filter((v) => hasPast(v.start_actual) || hasPast(v.start_scheduled))
     .map((v) => ({ ...v, started: hasPast(v.start_actual) }));
-  if (liveVideos.length === 0) {
+  const upCommingVideos = props.videos
+    .filter((v) => v.type !== 'clip')
+    .filter((v) => v.status === 'upcoming')
+    .map((v) => ({ ...v, started: hasPast(v.start_actual) }));
+  if (liveVideos.length === 0 && upCommingVideos.length === 0) {
     return (
       <div className="flex justify-center p-10">
         <EmptyImage message="Not found live video" />
@@ -55,7 +60,25 @@ const Videos = (props: VideoProps): JSX.Element => {
           }
         })}
       </div>
-
+      {liveVideos.length > 0 && upCommingVideos.length > 0 && (
+        <Divider className="my-4" />
+      )}
+      {upCommingVideos.length > 0 && (
+        <div className="flex flex-col md:flex-row flex-wrap">
+          {upCommingVideos.map((v) => {
+            switch (v.type) {
+              case 'stream':
+                return <VideoCardStream key={v.id} {...v} />;
+              case 'placeholder':
+                return <VideoCardPlaceholder key={v.id} {...v} />;
+              default:
+                return (
+                  <VideoCardSkeleton key={`skeleton-${crypto.randomUUID()}`} />
+                );
+            }
+          })}
+        </div>
+      )}
       <ScrollToTopButton />
     </>
   );
